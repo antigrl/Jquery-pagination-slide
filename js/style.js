c8aabe4e6f4df2fcc60c6db9ad6f1a2a89fe4fb2
Kim <kimwalta@gmail.com>
2013-02-27 15:28:45 -0600

    corrected slide direction and added font-awesome

--- js/style.js
+++ js/style.js
@@ -1,44 +1,54 @@
-    $(function() {
-    	var page=1;
-    	var pages=3;
+$(function() {
+	var page=1;
+	var pages=3;
 
-    	$('.arrow-left').click(function() {
-	    	$('<div class="right-box box"></div>').appendTo('.box-container');
+	$('.arrow-left').click(function() {
+    	$('<div class="left-box box"></div>').appendTo('.box-container');
 
-    		$('.center-box').animate({left:'-1450px'},500, function() {
-    			$('.center-box').remove();
-    		});
-    		$('.right-box').animate({left:'200px'},500, function() {
-				$('.right-box').addClass('center-box').removeClass('right-box');
-    		});
+		$('.center-box').animate({left:'1450px'},500, function() {
+			$('.center-box').remove();
+		});
+		$('.left-box').animate({left:'200px'},500, function() {
+			$('.left-box').addClass('center-box').removeClass('left-box');
+		});
 
-    		page--;
-    		if(page < 1) {
-    			page = 3;
-    		}
-	  		$('.page-marker').removeClass('active').addClass('page');
-	  		$('.page-control').children(':nth-child('+page+')').addClass('active').removeClass('page');
+		page--;
+		if(page < 1) {
+			page = 3;
+		}
+  		$('.page-marker').removeClass('active').addClass('page');
+  		$('.page-control').children(':nth-child('+page+')').addClass('active').removeClass('page');
 
-    	});
+	});
 
-    	$('.arrow-right').click(function() {
-    		$('<div class="left-box box"></div>').appendTo('.box-container');
+	$('.arrow-right').click(function() {
+        $('<div class="right-box box"></div>').appendTo('.box-container');
+		
+		$('.center-box').animate({left:'-1450px'},500, function() {
+			$('.center-box').remove();
+		});
+		$('.right-box').animate({left:'200px'},500, function() {
+			$('.right-box').addClass('center-box').removeClass('right-box');
 
-    		$('.center-box').animate({left:'1450px'},500, function() {
-    			$('.center-box').remove();
-    		});
-    		$('.left-box').animate({left:'200px'},500, function() {
-    			$('.left-box').addClass('center-box').removeClass('left-box');
+		});
 
-    		});
+		page++;
+		if(page > 3) {
+			page = 1;
+		}
+		$('.page-marker').removeClass('active').addClass('page');
+  		$('.page-control').children(':nth-child('+page+')').addClass('active').removeClass('page');
+	});
+});
 
-    		page++;
-    		if(page > 3) {
-    			page = 1;
-    		}
-    		$('.page-marker').removeClass('active').addClass('page');
-	  		$('.page-control').children(':nth-child('+page+')').addClass('active').removeClass('page');
-    	});
-
-        
+$(function() {
+    $('#button').click(function(e) { // Button which will activate our modal
+        $('#modal').reveal({ // The item which will be opened with reveal
+            animation: 'fade',                   // fade, fadeAndPop, none
+            animationspeed: 600,                 // how fast animtions are
+            closeonbackgroundclick: true,
+            dismissmodalclass: 'close'    // the class of a button or element that will close an open modal
+        });
+    return false;
     });
+});
